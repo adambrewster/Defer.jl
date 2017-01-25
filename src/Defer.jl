ced0326244b1f9852adaa812854101d70ee9d2d5
Adam Brewster <adam.brewster@ll.mit.edu>
2017-01-25 11:59:13 -0500

    push and pop without do syntax

--- src/Defer.jl
+++ src/Defer.jl
@@ -1,44 +1,61 @@
 module Defer
 
-export scope, defer, @defer, @!
+export push_scope!, pop_scope!, scope, defer, @defer, @!
 
-const current_scope = Ref{Nullable{Array}}(Nullable{Array}())
+const scopes = Any[]
 
-function scope(f)
-  old_scope = current_scope[]
-  current_scope[] = Nullable([])
-  ex = Nullable{CompositeException}()
-  try
-    f()
-  catch e
-    ex = Nullable(CompositeException([e]))
-  finally
-    this_scope = get(current_scope[])
-    current_scope[] = old_scope
-    for fin in this_scope
-      try
-        fin()
-      catch e
-        if isnull(ex)
-          ex = Nullable(CompositeException())
-        end
-        push!(get(ex).exceptions, e)
-      end
-    end
-    empty!(this_scope)
-    if !isnull(ex)
-      if length(get(ex)) == 1
-        rethrow(get(ex).exceptions[1])
-      else
-        throw(get(ex))
-      end
+function push_scope!()
+  push!(scopes, Any[])
+  length(scopes)
+end
+
+function pop_scope!(i::Int)
+  if i == length(scopes)
+    pop_scope!()
+  else
+    warn("Popping scope $(length(scopes)), expected $i")
+    while length(scopes) >= i
+      pop_scope!()
     end
   end
 end
 
+function pop_scope!(e::Nullable{Any}=Nullable{Any}())
+  exceptions = isnull(e) ? Any[] : Any[get(e)]
+  this_scope = pop!(scopes)
+  for fin in this_scope
+    try
+      fin()
+    catch e
+      push!(exceptions, e)
+    end
+  end
+  empty!(this_scope)
+  if !isempty(exceptions)
+    if length(exceptions) == 1
+      rethrow(exceptions[1])
+    else
+      throw(CompositeException(exceptions))
+    end
+  end
+  nothing
+end
+
+function scope(f)
+  push_scope!()
+  ex = Nullable{Any}()
+  try
+    f()
+  catch e
+    ex = Nullable{Any}(e)
+  finally
+    pop_scope!(ex)
+  end
+end
+
 function defer(fin)
-  if isnull(current_scope[]) error("defer can only be used within a scope") end
-  push!(get(current_scope[]), fin)
+  if isempty(scopes) error("defer can only be used within a scope") end
+  push!(last(scopes), fin)
   nothing
 end
 
